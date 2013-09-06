7defe66399f5fffc785f5be5596248b43bc40820
Akshay Hegde <akshay.hegde17@gmail.com>
2013-09-06 00:53:38 -0700

    Add rudimentary JavaScanner class

---
+++ Assignment2/src/wci/frontend/java/JavaScanner.java
@@ -0,0 +1,18 @@
+package wci.frontend.java;
+
+import wci.frontend.*;
+
+public class JavaScanner extends Scanner {
+
+    public JavaScanner(Source source)
+    {
+        super(source);
+    }
+
+    @Override
+    protected Token extractToken() throws Exception
+    {
+
+    }
+}
+
