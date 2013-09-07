36cd09a3a5a162770727bec4e0b97fb822eb572f
Akshay Hegde <akshay.hegde17@gmail.com>
2013-09-07 13:09:20 -0700

    Modified JavaScanner, added todos

--- Assignment2/src/wci/frontend/java/JavaScanner.java
+++ Assignment2/src/wci/frontend/java/JavaScanner.java
@@ -12,7 +12,51 @@ public class JavaScanner extends Scanner {
     @Override
     protected Token extractToken() throws Exception
     {
+        skipWhiteSpace();
 
+        Token token;
+        char currentChar = currentChar();
+
+        if (currentChar == EOF) {
+            token = new EofToken(source);
+        }
+        // bunch of else ifs here for other java tokens.
+        // TODO: modify frontend JavaToken
+        else {
+            token = new JavaErrorToken();
+        }
+        return token;
+    }
+
+    /**
+     * Skip whitespace characters by consuming them.  A comment is whitespace.
+     * @throws Exception if an error occurred.
+     */
+    private void skipWhiteSpace()
+            throws Exception
+    {
+        char currentChar = currentChar();
+
+        // TODO: Make comments whitespace for Java
+        while (Character.isWhitespace(currentChar) || (currentChar == '/')) {
+
+            // Start of a Java comment?
+            if (currentChar == '/') {
+                do {
+                    currentChar = nextChar();  // consume comment characters
+                } while ((currentChar != '}') && (currentChar != EOF));
+
+                // Found closing '}'?
+                if (currentChar == '}') {
+                    currentChar = nextChar();  // consume the '}'
+                }
+            }
+
+            // Not a comment.
+            else {
+                currentChar = nextChar();  // consume whitespace character
+            }
+        }
     }
 }
 
