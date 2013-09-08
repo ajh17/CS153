1d420839acca07d8c9268dcbdb5f380f3a69e2f0
Akshay Hegde <akshay.hegde17@gmail.com>
2013-09-08 13:39:38 -0700

    Added a basic JavaCharacterToken so that JavaScanner compiles without errors.

---
+++ Assignment2/src/wci/frontend/java/tokens/JavaCharacterToken.java
@@ -0,0 +1,12 @@
+package wci.frontend.java.tokens;
+
+import wci.frontend.Source;
+import wci.frontend.java.JavaToken;
+
+public class JavaCharacterToken extends JavaToken {
+
+    public JavaCharacterToken(Source source) throws Exception
+    {
+        super(source);
+    }
+}
