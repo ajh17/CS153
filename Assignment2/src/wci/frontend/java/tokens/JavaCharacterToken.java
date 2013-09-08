e84578e95db890c9a8adf5e5c61d31e1f0c3e77e
David Tang <dtangster@yahoo.com>
2013-09-08 16:22:31 -0700

    Added error checking to JavaCharacterToken.

--- Assignment2/src/wci/frontend/java/tokens/JavaCharacterToken.java
+++ Assignment2/src/wci/frontend/java/tokens/JavaCharacterToken.java
@@ -2,8 +2,6 @@ package wci.frontend.java.tokens;
 
 import wci.frontend.Source;
 import wci.frontend.java.JavaToken;
-import static wci.frontend.Source.EOL;
-import static wci.frontend.Source.EOF;
 import static wci.frontend.java.JavaTokenType.*;
 import static wci.frontend.java.JavaErrorCode.*;
 
@@ -30,11 +28,41 @@ public class JavaCharacterToken extends JavaToken {
 
         char currentChar = nextChar();  // consume initial quote
         textBuffer.append('\'');
-        textBuffer.append(currentChar);
-        textBuffer.append('\'');
-        nextChar(); // consume character
-        nextChar(); // consume ending quote
 
-        text = textBuffer.toString();
+        if (currentChar == '\\') {   // check for escape character
+            currentChar = nextChar();  // consume escape character
+
+            switch (currentChar) {
+                case '\\':
+                case '\'': textBuffer.append(currentChar); break;
+                case 'n': textBuffer.append('\n'); break;
+                case 't': textBuffer.append('\t'); break;
+
+                // An error if not a character in the switch statement
+                default:
+                    type = ERROR;
+                    value = INVALID_CHARACTER;
+                    text = String.valueOf(currentChar);
+            }
+        }
+
+        if (type != ERROR) {
+            textBuffer.append(currentChar); // append the character between single quotes
+            currentChar = nextChar();
+
+            if (currentChar == '\'') {
+                nextChar();  // consume final quote
+                textBuffer.append('\'');
+
+                type = CHARACTER;
+                value = textBuffer.toString();
+            }
+            else {
+                type = ERROR;
+                value = UNEXPECTED_EOF;
+            }
+
+            text = textBuffer.toString();
+        }
     }
 }
