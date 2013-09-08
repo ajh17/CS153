2fb143d9ee920a76e3ae3bd7422746fc2c8bf045
Akshay Hegde <akshay.hegde17@gmail.com>
2013-09-08 10:53:08 -0700

    Add an incomplete version of JavaSpecialSymbolToken.

--- Assignment2/src/wci/frontend/java/tokens/JavaSpecialSymbolToken.java
+++ Assignment2/src/wci/frontend/java/tokens/JavaSpecialSymbolToken.java
@@ -2,6 +2,8 @@ package wci.frontend.java.tokens;
 
 import wci.frontend.Source;
 import wci.frontend.java.JavaToken;
+import static wci.frontend.java.JavaErrorCode.*;
+import static wci.frontend.pascal.PascalTokenType.*;
 
 public class JavaSpecialSymbolToken extends JavaToken {
 
@@ -14,6 +16,68 @@ public class JavaSpecialSymbolToken extends JavaToken {
             throws Exception
     {
         super(source);
+    }
+
+    protected void extract() throws Exception
+    {
+        char currentChar = currentChar();
+
+        text = Character.toString(currentChar);
+        type = null;
+
+        switch (currentChar) {
+
+            // Single-character special symbols.
+            // There may be more for java
+            // TODO: Add any missing single-character special symbols
+            case '+':  case '-':  case '*':  case '/':  case ',':
+            case ';':  case '\'': case '=':  case '(':  case ')':
+            case '[':  case ']':  case '{':  case '}':  case '^':
+            case '&':  case '|':   case '.': case ':':  case '~':
+            {
+                nextChar();  // consume character
+                break;
+            }
+
+            // < or <= or <>
+            case '<': {
+                currentChar = nextChar();  // consume '<';
+
+                if (currentChar == '=') {
+                    text += currentChar;
+                    nextChar();  // consume '='
+                }
+                else if (currentChar == '>') {
+                    text += currentChar;
+                    nextChar();  // consume '>'
+                }
+
+                break;
+            }
+
+            // > or >=
+            case '>': {
+                currentChar = nextChar();  // consume '>';
+
+                if (currentChar == '=') {
+                    text += currentChar;
+                    nextChar();  // consume '='
+                }
+
+                break;
+            }
+
+            default: {
+                nextChar();  // consume bad character
+                type = ERROR;
+                value = INVALID_CHARACTER;
+            }
+        }
+
+        // Set the type if it wasn't an error.
+        if (type == null) {
+            type = SPECIAL_SYMBOLS.get(text);
+        }
     }
 
 }
