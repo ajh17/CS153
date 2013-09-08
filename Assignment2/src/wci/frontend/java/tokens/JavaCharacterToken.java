a834b3478bd1023943ca2c1654b7944a4c735e72
dtangster <dtangster@yahoo.com>
2013-09-08 15:54:34 -0700

    Update JavaCharacterToken.java

--- Assignment2/src/wci/frontend/java/tokens/JavaCharacterToken.java
+++ Assignment2/src/wci/frontend/java/tokens/JavaCharacterToken.java
@@ -2,11 +2,39 @@ package wci.frontend.java.tokens;
 
 import wci.frontend.Source;
 import wci.frontend.java.JavaToken;
+import static wci.frontend.Source.EOL;
+import static wci.frontend.Source.EOF;
+import static wci.frontend.java.JavaTokenType.*;
+import static wci.frontend.java.JavaErrorCode.*;
 
 public class JavaCharacterToken extends JavaToken {
 
-    public JavaCharacterToken(Source source) throws Exception
+    /**
+     * Constructor.
+     * @param source the source from where to fetch the token's characters.
+     * @throws Exception if an error occurred.
+     */
+    public JavaCharacterToken(Source source)
+            throws Exception
     {
         super(source);
+    }
+
+    /**
+     * Extract a Java character token from the source.
+     * @throws Exception if an error occurred.
+     */
+    protected void extract() throws Exception
+    {
+        StringBuilder textBuffer = new StringBuilder();
+
+        char currentChar = nextChar();  // consume initial quote
+        textBuffer.append('\'');
+        textBuffer.append(currentChar);
+        textBuffer.append('\'');
+        nextChar(); // consume character
+        nextChar(); // consume ending quote
+
+        text = textBuffer.toString();
     }
 }
