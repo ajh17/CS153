41f29673de23030642bd7bc9fe5b89b46a1accb1
Akshay Hegde <akshay.hegde17@gmail.com>
2013-09-19 12:26:11 -0700

    Create rudimentary parsers for Set expressions and set assignments

---
+++ Assignment3/src/wci/frontend/pascal/parsers/SetAssignmentStatement.java
@@ -0,0 +1,18 @@
+package wci.frontend.pascal.parsers;
+
+import wci.frontend.*;
+import wci.frontend.pascal.*;
+import wci.intermediate.*;
+
+public class SetAssignmentStatement extends AssignmentStatementParser {
+
+    public SetAssignmentStatement(PascalParserTD parent)
+    {
+        super(parent);
+    }
+
+    public ICodeNode parse(Token token) throws Exception
+    {
+
+    }
+}
---
+++ Assignment3/src/wci/frontend/pascal/parsers/SetExpressionParser.java
@@ -0,0 +1,16 @@
+package wci.frontend.pascal.parsers;
+
+import wci.frontend.*;
+import wci.frontend.pascal.*;
+import wci.intermediate.*;
+
+public class SetExpressionParser extends ExpressionParser {
+
+    public SetExpressionParser(PascalParserTD parent) {
+        super(parent);
+    }
+
+    public ICodeNode parse(Token token) throws Exception {
+
+    }
+}
