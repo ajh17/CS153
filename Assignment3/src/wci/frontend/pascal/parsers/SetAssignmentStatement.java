56c243f612bb425fa7c52109ba01ef4c97d7974b
Akshay Hegde <akshay.hegde17@gmail.com>
2013-09-19 12:42:17 -0700

    Implement some of the features of Pascal's SetAssignmentParser

--- Assignment3/src/wci/frontend/pascal/parsers/SetAssignmentStatement.java
+++ Assignment3/src/wci/frontend/pascal/parsers/SetAssignmentStatement.java
@@ -4,15 +4,67 @@ import wci.frontend.*;
 import wci.frontend.pascal.*;
 import wci.intermediate.*;
 
+import java.util.EnumSet;
+
+import static wci.frontend.pascal.PascalTokenType.*;
+import static wci.frontend.pascal.PascalErrorCode.*;
+import static wci.intermediate.icodeimpl.ICodeNodeTypeImpl.*;
+import static wci.intermediate.icodeimpl.ICodeKeyImpl.*;
+
 public class SetAssignmentStatement extends AssignmentStatementParser {
+
+    private static final EnumSet<PascalTokenType> EQUALS_SET = ExpressionParser.EXPR_START_SET.clone();
+
+    static {
+        EQUALS_SET.add(EQUALS);
+        EQUALS_SET.addAll(AssignmentStatementParser.STMT_FOLLOW_SET);
+    }
 
     public SetAssignmentStatement(PascalParserTD parent)
     {
         super(parent);
     }
 
+    /**
+     * Parse the set assignment statement.
+     * @param token the initial token.
+     * @return the root node of the generated parse tree.
+     * @throws Exception if an error occured
+     */
     public ICodeNode parse(Token token) throws Exception
     {
+        // Create the ASSIGN node
+        ICodeNode assignNode = ICodeFactory.createICodeNode(ASSIGN);
 
+        // Look up target identifier and add it to the stack if not found
+        String targetName = token.getText().toLowerCase();
+        SymTabEntry targetId = symTabStack.lookup(targetName);
+
+        if (targetId == null) {
+            targetId = symTabStack.enterLocal(targetName);
+        }
+        targetId.appendLineNumber(token.getLineNumber());
+
+        token = nextToken(); // Consume identifier
+
+        // Create the variable node and set its name attribute.
+        ICodeNode variableNode = ICodeFactory.createICodeNode(VARIABLE);
+        variableNode.setAttribute(ID, targetId);
+
+        assignNode.addChild(variableNode);
+
+        // Synchronization set for the = token.
+        token = synchronize(EQUALS_SET);
+        if (token.getType() == EQUALS) {
+            token = nextToken(); // consume the =
+        }
+        else {
+            errorHandler.flag(token, MISSING_EQUALS, this);
+        }
+
+        AssignmentStatementParser assignmentParser = new AssignmentStatementParser(this);
+        assignNode.addChild(assignmentParser.parse(token));
+
+        return assignNode;
     }
 }
