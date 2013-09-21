a9587cc73bbe224a4a7a38d9fb72735c49bf2835
Akshay Hegde <akshay.hegde17@gmail.com>
2013-09-20 19:34:33 -0700

    Refactor set expressions out of ExpressionParser to be more modular

--- Assignment3/src/wci/frontend/pascal/parsers/ExpressionParser.java
+++ Assignment3/src/wci/frontend/pascal/parsers/ExpressionParser.java
@@ -328,40 +328,8 @@ public class ExpressionParser extends StatementParser
 
             // Case statement for set expressions
             case LEFT_BRACKET: {
-                token = nextToken(); // Consume the [
-
-                rootNode = ICodeFactory.createICodeNode(ICodeNodeTypeImpl.SET); // This SET is different from PascalTokenType
-                HashSet<Integer> values = new HashSet<Integer>();
-
-                // NOT SURE IF THIS IS CORRECT. SHOULD WE ADD MULTIPLE CHILDREN WITH CONSTANT INTEGERS?
-                rootNode.setAttribute(VALUE, values);
-
-                // Crude way of extracting the set of numbers
-                do {
-                    if (token.getType() == INTEGER) {
-                        Integer number = (Integer) token.getValue(); // getValue() returns Object type. Need to recast.
-
-                        if (number >= 0 && number <= 50) {
-                            values.add(number);
-                        }
-                        else {
-                            errorHandler.flag(token, RANGE_INTEGER, this); // Report integer being out of range
-                        }
-                    }
-
-                    token = nextToken();
-                } while (token.getType() != RIGHT_BRACKET && token.getType() != ERROR);
-
-                if (token.getType() == ERROR) {
-                    errorHandler.flag(token, UNEXPECTED_EOF, this);
-                }
-                else if (token.getType() == RIGHT_PAREN) {
-                    token = nextToken();  // consume the ]
-                }
-                else {
-                    errorHandler.flag(token, MISSING_RIGHT_BRACKET, this);
-                }
-
+                SetExpressionParser setExprParser = new SetExpressionParser(this);
+                rootNode = setExprParser.parse(token);
                 break;
             }
 
--- Assignment3/src/wci/frontend/pascal/parsers/SetExpressionParser.java
+++ Assignment3/src/wci/frontend/pascal/parsers/SetExpressionParser.java
@@ -3,6 +3,9 @@ package wci.frontend.pascal.parsers;
 import wci.frontend.*;
 import wci.frontend.pascal.*;
 import wci.intermediate.*;
+import wci.intermediate.icodeimpl.ICodeNodeTypeImpl;
+
+import java.util.HashSet;
 
 public class SetExpressionParser extends ExpressionParser {
 
@@ -10,7 +13,43 @@ public class SetExpressionParser extends ExpressionParser {
         super(parent);
     }
 
-    public ICodeNode parse(Token token) throws Exception {
-        return null; // Added so build doesn't give error
+    public ICodeNode parse(Token token)
+            throws Exception
+    {
+        ICodeNode rootNode = null;
+        token = nextToken(); // Consume the [
+
+        rootNode = ICodeFactory.createICodeNode(ICodeNodeTypeImpl.SET); // This SET is different from PascalTokenType
+        HashSet<Integer> values = new HashSet<Integer>();
+
+        // Note: NOT SURE IF THIS IS CORRECT. SHOULD WE ADD MULTIPLE CHILDREN WITH CONSTANT INTEGERS?
+        rootNode.setAttribute(VALUE, values);
+
+        // Crude way of extracting the set of numbers
+        do {
+            if (token.getType() == INTEGER) {
+                Integer number = (Integer) token.getValue(); // getValue() returns Object type. Need to recast.
+
+                if (number >= 0 && number <= 50) {
+                    values.add(number);
+                }
+                else {
+                    errorHandler.flag(token, RANGE_INTEGER, this); // Report integer being out of range
+                }
+            }
+
+            token = nextToken();
+        } while (token.getType() != RIGHT_BRACKET && token.getType() != ERROR);
+
+        if (token.getType() == ERROR) {
+            errorHandler.flag(token, UNEXPECTED_EOF, this);
+        }
+        else if (token.getType() == RIGHT_PAREN) {
+            token = nextToken();  // consume the ]
+        }
+        else {
+            errorHandler.flag(token, MISSING_RIGHT_BRACKET, this);
+        }
+        return rootNode;
     }
 }
