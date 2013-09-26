c6505f3b3fcba199fe4765311cce14c63101803a
Akshay Hegde <akshay.hegde17@gmail.com>
2013-09-25 23:51:23 -0700

    nextToken should not be here

--- Assignment3/src/wci/frontend/pascal/parsers/ExpressionParser.java
+++ Assignment3/src/wci/frontend/pascal/parsers/ExpressionParser.java
@@ -418,7 +418,6 @@ public class ExpressionParser extends StatementParser {
                         subrangeNode.addChild(leftNumberNode);
                         subrangeNode.addChild(rightNumberNode);
                         rootNode.addChild(subrangeNode);
-                        token = nextToken();
                     }
 
                     break;
