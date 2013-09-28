bdf8e9baaaeea091accacaa0733dd2b4c5d302b9
Akshay Hegde <akshay.hegde17@gmail.com>
2013-09-27 23:10:01 -0700

    Revert f43dc42..df9b4e7
    This rolls back to commit f43dc42d56e3ccddbc72493eab7fad1de91ad496.

--- Assignment3/src/wci/backend/interpreter/executors/ExpressionExecutor.java
+++ Assignment3/src/wci/backend/interpreter/executors/ExpressionExecutor.java
@@ -231,8 +231,7 @@ public class ExpressionExecutor extends StatementExecutor {
                 case GE:
                     return value1 >= value2;
                 }
-            }
-            else if (setMode) {
+            } else if (setMode) {
                 HashSet<Integer> tempSet;
 
                 switch (nodeType) {
@@ -248,9 +247,26 @@ public class ExpressionExecutor extends StatementExecutor {
                         tempSet = makeHashSetCopy(set1);
                         tempSet.retainAll(set2);
                         return tempSet;
+                    case LE:  // S1 <= S2
+                        return set2.containsAll(set1);
+                    case GE:  // S1 >= S2
+                        return set1.containsAll(set2);
+                    case EQ: // S1 = S2
+                        return set1.containsAll(set2) && set2.containsAll(set1);
+                    case NE:  // S1 <> S2
+                        return !set1.containsAll(set2) || !set2.containsAll(set1);
+                    case IN_SET:  // s in S1
+                        // TODO: Figure out why this is not flagging error8 & error9 as errors.
+                        if (operand1 instanceof Integer) {
+                            return set2.contains(operand1);
+                        }
+                        errorHandler.flag(node, INVALID_OPERATOR, this);
+                        break;
+                    default:
+                        errorHandler.flag(node, INVALID_OPERATOR, this);
+                        break;
                 }
-            }
-            else {
+            } else {
                 float value1 = operand1 instanceof Integer ? (Integer) operand1 : (Float) operand1;
                 float value2 = operand2 instanceof Integer ? (Integer) operand2 : (Float) operand2;
                 // Float operations.
@@ -308,31 +324,8 @@ public class ExpressionExecutor extends StatementExecutor {
                     return value1 > value2;
                 case GE:
                     return value1 >= value2;
-                default:
-                    errorHandler.flag(node, INVALID_OPERATOR, this);
-                    return 0;
             }
-        }
-        else if (setMode) {
-            switch (nodeType) {
-                case LE:  // S1 <= S2
-                    return set2.containsAll(set1);
-                case GE:  // S1 >= S2
-                    return set1.containsAll(set2);
-                case EQ: // S1 = S2
-                    return set1.containsAll(set2) && set2.containsAll(set1);
-                case NE:  // S1 <> S2
-                    return !set1.containsAll(set2) || !set2.containsAll(set1);
-                case IN_SET:  // s in S1
-                    // TODO: Figure out why this is not flagging error8 & error9 as errors.
-                    if (operand1 instanceof Integer) {
-                        return set2.contains(operand1);
-                    }
-                    errorHandler.flag(node, INVALID_OPERATOR, this);
-                    break;
-            }
-        }
-        else {
+        } else {
             float value1 = operand1 instanceof Integer
                     ? (Integer) operand1 : (Float) operand1;
             float value2 = operand2 instanceof Integer
