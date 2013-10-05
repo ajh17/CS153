c13fd253370bed39f297153369648364ca46eac0
David Tang <dtangster@yahoo.com>
2013-10-05 14:07:04 -0700

    Added some basic structure to SetTypeParser.

--- Assignment4/src/wci/frontend/pascal/parsers/SetTypeParser.java
+++ Assignment4/src/wci/frontend/pascal/parsers/SetTypeParser.java
@@ -3,7 +3,10 @@ package wci.frontend.pascal.parsers;
 import wci.frontend.Token;
 import wci.frontend.pascal.PascalParserTD;
 import wci.frontend.pascal.PascalTokenType;
+import wci.intermediate.TypeFactory;
 import wci.intermediate.TypeSpec;
+
+import static wci.intermediate.typeimpl.TypeFormImpl.ENUMERATION;
 
 public class SetTypeParser extends TypeSpecificationParser {
     protected SetTypeParser(PascalParserTD parent)
@@ -19,8 +22,19 @@ public class SetTypeParser extends TypeSpecificationParser {
         // Synchronize to what?
         // token = synchronize();
 
-        switch ((PascalTokenType) token.getType()) {
+        // TODO: Verify that ENUMBERATION is the correct type for SET
+        TypeSpec setType = TypeFactory.createType(ENUMERATION);
+        token = nextToken();  // consume SET
 
+        if (token.getType() == PascalTokenType.OF)  {
+            token = nextToken(); // consume OF
+
+            switch ((PascalTokenType) token.getType()) {
+
+            }
+        }
+        else {
+            // Invalid syntax for SET
         }
 
         return null; // Temporary to prevent build error
