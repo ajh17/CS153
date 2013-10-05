0bf29e17e667a00bfa83b451e6ab5c6c45699c68
David Tang <dtangster@yahoo.com>
2013-10-05 14:13:10 -0700

    Added TypeFormImpl.SET and used this instead of TypeFormImpl.ENUMERATION

--- Assignment4/src/wci/frontend/pascal/parsers/SetTypeParser.java
+++ Assignment4/src/wci/frontend/pascal/parsers/SetTypeParser.java
@@ -6,7 +6,7 @@ import wci.frontend.pascal.PascalTokenType;
 import wci.intermediate.TypeFactory;
 import wci.intermediate.TypeSpec;
 
-import static wci.intermediate.typeimpl.TypeFormImpl.ENUMERATION;
+import static wci.intermediate.typeimpl.TypeFormImpl.SET;
 
 public class SetTypeParser extends TypeSpecificationParser {
     protected SetTypeParser(PascalParserTD parent)
@@ -22,8 +22,7 @@ public class SetTypeParser extends TypeSpecificationParser {
         // Synchronize to what?
         // token = synchronize();
 
-        // TODO: Verify that ENUMBERATION is the correct type for SET
-        TypeSpec setType = TypeFactory.createType(ENUMERATION);
+        TypeSpec setType = TypeFactory.createType(SET);
         token = nextToken();  // consume SET
 
         if (token.getType() == PascalTokenType.OF)  {
