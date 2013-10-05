cdf63c67d756be2763f25a0156110cb025922a87
David Tang <dtangster@yahoo.com>
2013-10-05 16:59:20 -0700

    Normalized code styling

--- Assignment4/src/wci/frontend/pascal/parsers/SetTypeParser.java
+++ Assignment4/src/wci/frontend/pascal/parsers/SetTypeParser.java
@@ -13,14 +13,12 @@ import static wci.frontend.pascal.PascalErrorCode.IDENTIFIER_UNDEFINED;
 import static wci.intermediate.typeimpl.TypeFormImpl.SET;
 
 public class SetTypeParser extends TypeSpecificationParser {
-    protected SetTypeParser(PascalParserTD parent)
-    {
+
+    protected SetTypeParser(PascalParserTD parent) {
         super(parent);
     }
 
-    public TypeSpec parse(Token token)
-            throws Exception
-    {
+    public TypeSpec parse(Token token) throws Exception {
         // Synchronize at the start of a type specification.
 
         // Synchronize to what?
