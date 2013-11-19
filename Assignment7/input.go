19fc06262a6c94dbe5360a8e59ed71c557094dc8
Akshay Hegde <akshay.hegde17@gmail.com>
2013-11-19 14:12:26 -0800

    Change input file to prevent null pointer exceptions for now.

--- Assignment7/input.go
+++ Assignment7/input.go
@@ -3,41 +3,14 @@ var x int
 var foo float;
 var q float;
 var r string
-x++
---x;
-a = 5 + 5 + 5 + 5;
-b = 6
-c = 7.8
-d = 9.0
-e = "\tasdf\n"
-f = "\\ghjk\"";
-g = `\n\n\tabc\t\t`
-h = `\r\n"""""def"`;
 
 if true {
+    var a int;
     a = 5;
+    var b int;
     b = 6
-    c = 7.0
+    var c int;
+    c = 7.0;
+    var d int;
     d = "hello\n";
-}
-
-if a > b {
-    a = b;
-}
-
-for i = 0; i < 10; i++ {
-    e = "hello\n";
-}
-
-switch {
-    case true: 
-	case false: 
-    default: 
-}
-
-i = 5
-switch i {
-    case 4: 
-    case 5: 
-    case 6:
 }
