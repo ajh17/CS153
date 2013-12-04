4bde554550ad4fd3211e5a88167e4a02144116d5
Akshay Hegde <akshay.hegde17@gmail.com>
2013-12-03 22:24:10 -0800

    Add David's tests to inputError.go instead of the main input.go file

--- Assignment7/inputErrors.go
+++ Assignment7/inputErrors.go
@@ -1,12 +1,5 @@
 package main
-var x int
-var foo; // no type
-var float; // no variable name
-var didFallDown boolean; // no boolean type; called bool
-
-if true { // all this is valid and should be in cross ref table
-    var a int;
-    a = true; 
-    var d int;
-    d = 5;
-}
+var dsind int int;
+var int abc;
+var var asjd12;
+var y int;
