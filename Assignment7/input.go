f828a989984f2b5c2f6de444a5805fbf856e43ad
Akshay Hegde <akshay.hegde17@gmail.com>
2013-12-04 18:33:32 -0800

    Revert input.go file to its previous state.

--- Assignment7/input.go
+++ Assignment7/input.go
@@ -1,6 +1,18 @@
 package main
+var x int;
+var foo float;
+var q float;
+var r string;
+//var didFallDown bool;
+//didFallDown = true;
 
 if true {
     var a int;
     a = 5;
+    var b int;
+    b = 6;
+    var c float;
+    c = 7.0;
+    var d string;
+    d = "hello\n";
 }
