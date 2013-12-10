c1745cd26b25b224c940ba4c76e45b0013f680a6
Akshay Hegde <akshay.hegde17@gmail.com>
2013-12-10 15:22:44 -0800

    Add for loop to input file for testing

--- Assignment7/input.go
+++ Assignment7/input.go
@@ -2,8 +2,13 @@ package main
 var x float;
 var y bool;
 x = 1 + 2 + 1.2 + 1.5 + 2 + 4 + 4.5;
-y = true;
+y = false;
 if y {
     Println("hello world!");
     Println("Testing..1..2...3");
+}
+
+var loop int;
+for (loop = 0; loop <= 2; loop++) {
+
 }
