91902561ec8b5229139112777808f0bd873c0360
David Tang <dtangster@yahoo.com>
2013-12-10 14:01:33 -0800

    Added if statement to input.go for testing.

--- Assignment7/input.go
+++ Assignment7/input.go
@@ -1,7 +1,9 @@
 package main
-var y bool;
 var x float;
+var y bool;
 x = 1 + 2 + 1.2 + 1.5 + 2 + 4 + 4.5;
 y = true;
-Println("hello world!");
-Println("Testing..1..2...3");
+if y {
+    Println("hello world!");
+    Println("Testing..1..2...3");
+}
