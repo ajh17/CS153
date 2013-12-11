970b680463f1e04b009e22f972b20d5fe0ef3021
David Tang <dtangster@yahoo.com>
2013-12-11 10:25:39 -0800

    Shortened IF-ELSE test case in input file.

--- Assignment7/input.go
+++ Assignment7/input.go
@@ -8,40 +8,14 @@ var b float;
 a = -4;
 b = -5.102;
 
-// Relational operator only works for integers at the moment
-if x < 5 {
-    Println("hello world!");
-    Println("Testing..1..2...3");
-}
-
 if 5 < 6 {
     Println("5 < 6");
-
-    if 5 <= 5 {
-        Println("5 <= 5");
-    }
 
     if 3 > 5 {
         Println("err: 3 > 5");
     }
     else {
         Println("else from: 3 > 5");
-    }
-
-    if 10.5 <= 11 {
-        Println("10.5 <= 11");
-
-        if 12.5 >= 12.5 {
-            Println("12.5 >= 12.5");
-
-            if 5 != 5.0 {
-                Println("err: 5 != 5.0");
-            }
-        }
-
-        if 10 > 10.1 {
-            Println("err: 10 > 10.1");
-        }
     }
 }
 else if 4 == 4 {
