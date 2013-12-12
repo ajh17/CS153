c5f1725e6976aa9dbb6d78f9ed065f98faebaf50
David Tang <dtangster@yahoo.com>
2013-12-12 12:06:45 -0800

    Added another simple test case program.

--- Assignment7/input.go
+++ Assignment7/input.go
@@ -1,5 +1,9 @@
 package main
 var i int;
+var quarter int;
+var dime int;
+var nickel int;
+var penny int;
 
 for i = 1; i <= 100; i = i + 1 {
     Println(i);
@@ -10,6 +14,26 @@ for i = 1; i <= 100; i = i + 1 {
 
     if i % 5 == 0 {
         Println("buzz");
+    }
+}
+
+for quarter = 0; quarter <= 4; quarter = quarter + 1 {
+    for dime = 0; dime <= 10; dime = dime + 1 {
+        for nickel = 0; nickel <= 20; nickel = nickel + 1 {
+            for penny = 0; penny <= 100; penny = penny + 1 {
+                if quarter * 25 + dime * 10 + nickel * 5 + penny == 100 {
+                    Println("These make up a dollar");
+                    Println("Quarters:");
+                    Println(quarter);
+                    Println("Dimes:");
+                    Println(dime);
+                    Println("Nickels:");
+                    Println(nickel);
+                    Println("Pennies:");
+                    Println(penny);
+                }
+            }
+        }
     }
 }
 
