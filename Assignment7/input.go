b25109b9084f3cee860a0db52d43f4d785eb74d9
David Tang <dtangster@yahoo.com>
2013-12-13 22:00:10 -0800

    Check nesting level in Go.jjt instead of using a flag. Also changed input.go and trying to make it run.

--- Assignment7/input.go
+++ Assignment7/input.go
@@ -1,26 +1,30 @@
 package main
-i int;
-i = 5;
 
-func printStuff(x int) void {
-    Println("Inside function");
-    x = 20;
-    Println(x);
-    Println(i);
+func getDollarPermutations() void {
+    quarter int;
+    dime int;
+    nickel int;
+    penny int;
 
-
-
-    Println("Leaving function");
+    for quarter = 0; quarter <= 4; quarter = quarter + 1 {
+        for dime = 0; dime <= 10; dime = dime + 1 {
+            for nickel = 0; nickel <= 20; nickel = nickel + 1 {
+                for penny = 0; penny <= 100; penny = penny + 1 {
+                    if quarter * 25 + dime * 10 + nickel * 5 + penny == 100 {
+                        Println("These make up a dollar");
+                        Println("Quarters:");
+                        Println(quarter);
+                        Println("Dimes:");
+                        Println(dime);
+                        Println("Nickels:");
+                        Println(nickel);
+                        Println("Pennies:");
+                        Println(penny);
+                    }
+                }
+            }
+        }
+    }
 }
 
-printStuff(i);
-i = 10;
-printStuff(i);
-Println(i);
-
-if i == 10 {
-    Println("i = 10");
-}
-else {
-    Println("i != 10");
-}
+getDollarPermutations();
