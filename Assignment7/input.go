64e4d1a0342da0b007f3de361c9ad1f9c8bf5b2e
Akshay Hegde <akshay.hegde17@gmail.com>
2013-12-14 01:05:44 -0800

    Change input.go to use the new increment operator

--- Assignment7/input.go
+++ Assignment7/input.go
@@ -6,10 +6,10 @@ func getDollarPermutations() void {
     nickel int;
     penny int;
 
-    for quarter = 0; quarter <= 4; quarter = quarter + 1 {
-        for dime = 0; dime <= 10; dime = dime + 1 {
-            for nickel = 0; nickel <= 20; nickel = nickel + 1 {
-                for penny = 0; penny <= 100; penny = penny + 1 {
+    for quarter = 0; quarter <= 4; quarter++ {
+        for dime = 0; dime <= 10; dime++ {
+            for nickel = 0; nickel <= 20; nickel++ {
+                for penny = 0; penny <= 100; penny++ {
                     if quarter * 25 + dime * 10 + nickel * 5 + penny == 100 {
                         Println("These make up a dollar");
                         Println("Quarters:");
