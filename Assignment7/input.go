4999dd3506803a54f70413077f8b9c84cdd402c1
David Tang <dtangster@yahoo.com>
2013-12-15 10:33:49 -0800

    Fixed logic error in errorHandler

--- Assignment7/input.go
+++ Assignment7/input.go
@@ -1,46 +1,13 @@
 package main
 
-func getDollarPermutations() void {
-    quarter int;
-    dime int;
-    nickel int;
-    penny int;
+x int;
+x = 10;
 
-    for quarter = 0; quarter <= 4; quarter++ {
-        for dime = 0; dime <= 10; dime++ {
-            for nickel = 0; nickel <= 20; nickel++ {
-                for penny = 0; penny <= 100; penny++ {
-                    if quarter * 25 + dime * 10 + nickel * 5 + penny == 100 {
-                        Println("These make up a dollar");
-                        Println("Quarters:");
-                        Println(quarter);
-                        Println("Dimes:");
-                        Println(dime);
-                        Println("Nickels:");
-                        Println(nickel);
-                        Println("Pennies:");
-                        Println(penny);
-                    }
-                }
-            }
-        }
-    }
+// Need to work on pass by reference
+func passByReference(x int) void {
+    x = 5;
+    Println(x);
 }
 
-func fizzbuzz() void {
-    i int;
-    for i = 1; i <= 100; i++ {
-        Println(i);
-
-        if i % 3 == 0 {
-            Println("fizz");
-        }
-
-        if i % 5 == 0 {
-            Println("buzz");
-        }
-    }
-}
-
-getDollarPermutations();
-fizzbuzz();
+passByReference(x);
+Println(x);
