a597e04c0f54bd904d95c0ffe64335a66b96d2e7
David Tang <dtangster@yahoo.com>
2013-12-15 18:35:26 -0800

    Added pclError.jjt for reference on how to do error handling.

--- Assignment7/input.go
+++ Assignment7/input.go
@@ -1,17 +1,13 @@
 package main
 
-func GoMain() void {
-    x int;
-    x = 10;
+x int;
+x = 10;
 
-    // Need to work on pass by reference
-    func passByReference(y *int) void {
-        y = 5;
-        Println(y);
-    }
-
-    passByReference(x);
-    Println(x);
+// Need to work on pass by reference
+func passByReference(y *int) void {
+    y = 5;
+    Println(y);
 }
 
-GoMain();
+passByReference(x);
+Println(x);
