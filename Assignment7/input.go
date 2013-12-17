16b1a12ad6252c7cebade155b352526c5f4f88c6
Akshay Hegde <akshay.hegde17@gmail.com>
2013-12-16 22:26:18 -0800

    Restore input.go

--- Assignment7/input.go
+++ Assignment7/input.go
@@ -24,51 +24,7 @@ func passByReference(a *int, b float, c *float) void {
     Println("Outside function");
 }
 
-func fizzbuzz() void {
-    i int;
-    for i = 1; i <= 100; i++ {
-        Println(i);
 
-        if i % 3 == 0 {
-            Println("fizz");
-        }
-
-        if i % 5 == 0 {
-            Println("buzz");
-        }
-    }
-}
-
-
-func getDollarPermutations() void {
-    quarter int;
-    dime int;
-    nickel int;
-    penny int;
-
-    for quarter = 0; quarter <= 4; quarter++ {
-        for dime = 0; dime <= 10; dime++ {
-            for nickel = 0; nickel <= 20; nickel++ {
-                for penny = 0; penny <= 100; penny++ {
-                    if quarter * 25 + dime * 10 + nickel * 5 + penny == 100 {
-                        Println("These make up a dollar");
-                        Println("Quarters:");
-                        Println(quarter);
-                        Println("Dimes:");
-                        Println(dime);
-                        Println("Nickels:");
-                        Println(nickel);
-                        Println("Pennies:");
-                        Println(penny);
-                    }
-                }
-            }
-        }
-    }
-}
-
-getDollarPermutations();
-fizzbuzz();
 passByReference(x, y, z);
 Println(x);
 Println(y);
